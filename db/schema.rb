11afeee0035dfaf63a997fcb61f2887c318afe2f
Atchyut <atchyutnagabhairava@gmail.com>
2018-11-14 09:58:20 +0530

    title: One-to-one association
    desc: Added Address table and associated it with the user table using has_one association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_033534) do
+ActiveRecord::Schema.define(version: 2018_11_14_042104) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "street"
+    t.string "city"
+    t.string "country"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
